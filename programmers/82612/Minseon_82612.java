cf1293bdf0abd24c2411c094d2b2d3d42ad1b0c4
SoleiI <kmssun0524@sookmyung.ac.kr>
2023-10-16 18:32:09 +0900

    10/16 김민선 프로그래머스 82612 문제 풀이

---
+++ programmers/82612/Minseon_82612.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+
+        for (int i = 1; i <= count; i++) {
+            answer += price * i;
+        }
+
+        if (answer > money) return answer - money;
+        else return 0;
+    }
+}
