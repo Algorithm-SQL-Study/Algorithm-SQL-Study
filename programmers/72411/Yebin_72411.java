653e665567afec9fa096b508ccc862fac7ea4e5c
yebin <dev.devin.kr@gmail.com>
2023-10-19 23:52:58 +0900

    REFACTOR 2023-10-19 programmers 72411
    - 생각해보니 맵을 이중으로 쓸 필요가 없음. 코스 개수 별로 조합 만들고 바로 가장 많이 시킨 세트를 찾기 때문.

--- programmers/72411/Yebin_72411.java
+++ programmers/72411/Yebin_72411.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 
 class Solution {
-    static Map<Integer, Map<String, Integer>> setMenu;
 
     public String[] solution(String[] orders, int[] course) {
         List<String> answer = new ArrayList<>();
 
-        setMenu = new HashMap<>();
         for (int c : course) {
-            setMenu.put(c, new HashMap<>());
+            Map<String, Integer> setMenu = new HashMap<>();
+
             // 메뉴 조합별 개수 세기
             for (String o : orders) {
                 char[] order = o.toCharArray();
                 Arrays.sort(order);
-                combination(order, 0, c, new StringBuilder());
+                combination(order, 0, c
+                        , new StringBuilder()
+                        , setMenu);
             }
 
             // 가장 많이 주문 된 세트
             int maxCount = 0;
             List<String> candidate = new ArrayList<>();
             for (Map.Entry<String, Integer> entry
-                    : setMenu.get(c).entrySet()) {
+                    : setMenu.entrySet()) {
 
                 if (entry.getValue() < 2) continue;
 
@@ -49,19 +50,18 @@ class Solution {
     private void combination(char[] order
             , int temp
             , int count
-            , StringBuilder sb) {
+            , StringBuilder sb
+            , Map<String, Integer> setMenu) {
         if (sb.length() == count) {
             String combi = sb.toString();
-            int saved = setMenu.get(count)
-                    .getOrDefault(combi, 0);
-            setMenu.get(count)
-                    .put(combi, saved + 1);
+            int saved = setMenu.getOrDefault(combi, 0);
+            setMenu.put(combi, saved + 1);
             return;
         }
 
         for (int i = temp; i < order.length; i++) {
             sb.append(order[i]);
-            combination(order, i + 1, count, sb);
+            combination(order, i + 1, count, sb, setMenu);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
