f6e0737bf2d92c4897babde5156faf5367c0db6f
yebin <dev.devin.kr@gmail.com>
2023-11-02 23:25:03 +0900

    2023-11-02 programmers 17680

---
+++ programmers/17680/Yebin_17680.java
@@ -0,0 +1,30 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+
+        int second = 0;
+        Deque<String> cache = new LinkedList<>();
+
+        for (String city : cities) {
+            city = city.toLowerCase();
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.add(city);
+                second += 1;
+            } else {
+                if (cache.size() >= cacheSize) {
+                    cache.poll();
+                }
+                cache.add(city);
+                second += 5;
+            }
+        }
+
+        return second;
+    }
+}
