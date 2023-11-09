8f7d16c73b0d49c48aa16d3be799a81570b85bac
yebin-choi <69137469+yebin-choi@users.noreply.github.com>
2023-11-09 22:43:58 +0900

    [개별 - Yebin] 12973 (#43)
    * 2023-11-03 programmers 12973
    
    * 2023-11-03 programmers 12973

---
+++ programmers/12973/Yebin_12973.java
@@ -0,0 +1,27 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.add(c);
+                continue;
+            }
+            if (c == stack.getLast()) {
+                stack.removeLast();
+                continue;
+            }
+            stack.add(c);
+        }
+
+        return stack.isEmpty()? 1: 0;
+    }
+}
