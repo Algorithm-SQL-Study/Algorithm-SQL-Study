655ac9109049d2434a5ac0233553d80f8450e25e
yebin-choi <69137469+yebin-choi@users.noreply.github.com>
2023-11-18 01:31:37 +0900

    [공통 - Yebin] 77486 (#52)
    * 2023-11-03 programmers 77486
    
    * 2023-11-03 programmers 77486
    
    * 2023-11-03 programmers 77486

---
+++ programmers/77486/Yebin_77486.java
@@ -0,0 +1,76 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral
+            , String[] seller, int[] amount) {
+
+        Map<String, String> pyramid = new HashMap<>();
+        Map<String, Integer> profit = new HashMap<>();
+
+        initPyramid(enroll, referral, pyramid);
+        initProfit(enroll, profit);
+        carculateProfit(seller, amount, pyramid, profit);
+
+        return printProfit(enroll, profit);
+    }
+
+    private void initPyramid(String[] enroll, String[] referral
+            , Map<String, String> pyramid) {
+        for (int i = 0; i < enroll.length; i++) {
+            String lower = enroll[i];
+            String higher = referral[i];
+
+            if (higher.equals("-")) continue;
+
+            pyramid.put(lower, higher);
+        }
+    }
+
+    private void initProfit(String[] enroll
+            , Map<String, Integer> profit) {
+        for (String person : enroll) {
+            profit.put(person, 0);
+        }
+    }
+
+    private void carculateProfit(String[] seller, int[] amount
+            , Map<String, String> pyramid
+            , Map<String, Integer> profit) {
+        for (int i = 0; i < seller.length; i++) {
+            String person = seller[i];
+            int money = amount[i] * 100;
+            giveCredit(person, money, pyramid, profit);
+        }
+    }
+
+    private void giveCredit(String person, int money
+            , Map<String, String> pyramid
+            , Map<String, Integer> profit) {
+        if (money == 0) return; // <- 이게 없으면 tc 11,12,13 시간초과
+
+        String higher = pyramid.get(person);
+        int credit = credit10percent(money);
+        profit.put(person, profit.get(person) + money - credit);
+
+        if (higher == null) return;
+
+        giveCredit(higher, credit, pyramid, profit);
+    }
+
+    private int credit10percent(int money) {
+        return money / 10;
+    }
+
+    private int[] printProfit(String[] enroll
+            , Map<String, Integer> profit) {
+
+        int[] answer = new int[enroll.length];
+
+        for (int i = 0; i < enroll.length; i++) {
+            answer[i] = profit.get(enroll[i]);
+        }
+
+        return answer;
+    }
+}
