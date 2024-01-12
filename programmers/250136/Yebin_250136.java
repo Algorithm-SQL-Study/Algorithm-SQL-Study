a63a36be38708ea836ee3eddf6b0d1e83594e5d6
yebin-choi <69137469+yebin-choi@users.noreply.github.com>
2024-01-13 00:37:51 +0900

    Update Yebin_250136.java

--- programmers/250136/Yebin_250136.java
+++ programmers/250136/Yebin_250136.java
@@ -8,53 +8,67 @@ class Solution {
     private Map<Integer, Integer> oilReserves;
     private int n;
     private int m;
-    
+
     public int solution(int[][] land) {
         int answer = 0;
         n = land.length;
         m = land[0].length;
-        
+
         oilList = new ArrayList<>();
         for (int j = 0; j < m; j++) oilList.add(new HashSet<>());
         oilReserves = new HashMap<>();
-        
+
         int oilId = -1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (land[i][j] == 1) dfs(land, i, j, oilId--);
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                if (land[i][j] == 1) {
+                    dfs(land, i, j, oilId);
+                    oilId--;
+                }
             }
         }
-        
+
         return calculateMaxOil();
     }
-    
-    private void dfs(int[][] land, int x, int y, int oilId) {
-        land[x][y] = oilId;
-        oilList.get(y).add(oilId);
-        oilReserves.put(oilId, oilReserves.getOrDefault(oilId, 0) + 1);
-        
-        for (int i = 0; i < 4; i++) {
-            int nx = x + diff[i][0];
-            int ny = y + diff[i][1];
-            
-            if (nx < 0 || nx > n-1 || ny < 0 || ny > m-1 
-               || land[nx][ny] == 0 || land[nx][ny] != 1) continue;
-            
-            dfs(land, nx, ny, oilId);
+
+    private void dfs(int[][] land, int startX, int startY, int oilId) {
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{startX, startY});
+
+        while (!stack.isEmpty()) {
+            int[] current = stack.pop();
+            int x = current[0];
+            int y = current[1];
+
+            if (land[x][y] != 1) continue;
+
+            land[x][y] = oilId;
+            oilList.get(y).add(oilId);
+            oilReserves.put(oilId, oilReserves.getOrDefault(oilId, 0) + 1);
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + diff[i][0];
+                int ny = y + diff[i][1];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && land[nx][ny] == 1) {
+                    stack.push(new int[]{nx, ny});
+                }
+            }
         }
     }
-    
+
     private int calculateMaxOil() {
         int answer = 0;
         for (Set<Integer> col : oilList) {
-            if (col.isEmpty()) continue;
-            
-            int temp = 0;
-            for (int oilId : col) temp += oilReserves.get(oilId);
-            
-            answer = Math.max(temp, answer);
+            if (!col.isEmpty()) {
+                int temp = 0;
+                for (int oilId : col) {
+                    temp += oilReserves.get(oilId);
+                }
+                answer = Math.max(temp, answer);
+            }
         }
-        
+
         return answer;
     }
 }
